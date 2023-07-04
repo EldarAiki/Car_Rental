489cddcf0357558504ed47774de7d3f8ff006a05
Eldar Nir <loshinas@gmail.com>
2023-07-04 18:48:26 +0300

    return to last version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.imagin.studio']
+        domains: ['cdn.imagin.studio', 'https://car-rental-showcase-mu.vercel.app/_next/image?url=https%3A%2F%2Fcdn.imagin.studio']
     },
     typescript: {
         ignoreBuildErrors: true,
