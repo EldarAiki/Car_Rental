21b095f0f8715e30737e4ca974dc2564dfdc3032
Eldar Nir <loshinas@gmail.com>
2023-07-05 14:59:02 +0300

    change config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
     images: {
         domains: ['cdn.imagin.studio', 'https://car-rental-showcase-mu.vercel.app/_next/image?url=https%3A%2F%2Fcdn.imagin.studio'],
-        protocol: 'https',
-        hostname: 'cdn.imagin.studio',
         unoptimized: true,
     },
     typescript: {
