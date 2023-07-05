630d000d6ffafbe3619e1bf7838e5be2001c0199
Eldar Nir <loshinas@gmail.com>
2023-07-05 14:54:03 +0300

    unoptimize images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.imagin.studio', 'https://car-rental-showcase-mu.vercel.app/_next/image?url=https%3A%2F%2Fcdn.imagin.studio']
+        domains: ['cdn.imagin.studio', 'https://car-rental-showcase-mu.vercel.app/_next/image?url=https%3A%2F%2Fcdn.imagin.studio'],
+        protocol: 'https',
+        hostname: 'cdn.imagin.studio',
+        unoptimized: true,
     },
     typescript: {
         ignoreBuildErrors: true,
